40529326d0b962d5f8d320ad3c1dadb55ae077fd
emmanueluwa <emmanuel@fulodev.com>
2024-05-15 14:46:01 +0100

    adding model for products

---
+++ src/models/product.ts
@@ -0,0 +1,58 @@
+import { Schema } from "mongoose";
+import { model } from "mongoose";
+import { Document } from "mongoose";
+import categories from "src/utils/categories";
+
+type productImage = { url: string; id: string };
+
+export interface ProductDocument extends Document {
+  owner: Schema.Types.ObjectId;
+  name: string;
+  price: number;
+  purchasingDate: Date;
+  category: string;
+  images: productImage[];
+  thumbnail: string;
+  description: string;
+}
+
+const schema = new Schema<ProductDocument>(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [...categories],
+      required: true,
+    },
+    images: [
+      {
+        type: Object,
+        url: String,
+        id: String,
+      },
+    ],
+    thumbnail: String,
+  },
+  { timestamps: true }
+);
+
+const ProductModel = model("Product", schema);
+export default ProductModel;
